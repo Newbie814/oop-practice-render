651f6186988d3d7c88f629027b2097d6caed9cc6
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-04 23:59:48 -0400

    added classes for shop, cart, single product, and beginning rendering for shopping cart

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,49 +1,125 @@
-const productList = {
-  products: [
-    {
-      title: 'Trip to a Black Hole',
-      imageUrl:
-        'https://res.cloudinary.com/dylvkdabj/image/upload/v1648871780/website%20pics%20family/black_hole_banner_mpkvhc.jpg',
-      price: '$100,000.73',
-      description: 'A crushing experience.',
-    },
-    {
-      title: 'Fan Ride with the Guardians of the Galaxy',
-      imageUrl:
-        'https://res.cloudinary.com/dylvkdabj/image/upload/v1646514283/website%20pics%20family/wp2647127-space-wallpaper-hd_irkddn.jpg',
-      price: '$100,000,765.73',
-      description: 'A side splitting adventure.',
-    },
-    {
-      title: 'Mosey on down to Natchez, Mississippi',
-      imageUrl:
-        'https://res.cloudinary.com/dylvkdabj/image/upload/v1649113933/website%20pics%20family/27natchez1-jumbo_xcei5l.jpg',
-      price: '$00.03',
-      description: 'An ambien like affair',
-    },
-  ],
-  render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for (const product of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-      <div>
-        <img src="${product.imageUrl}" alt="${product.title}">
-        <div class="product-item__content">
-          <h2>${product.title}</h2>
-          <h3>\$${product.price}</h3>
-          <p>${product.description}</p>
-          <button>Add to Cart</button>
+class Product {
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
 
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
+  render() {
+    const cartElement = document.createElement('section');
+    cartElement.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order</button>
+      `;
+    cartElement.className = 'cart';
+    this.totalOutput = cartElement.querySelector('h2');
+    return cartElement;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const productElement = document.createElement('li');
+    productElement.className = 'product-item';
+    productElement.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
          </div>
       </div>`;
-      prodList.append(prodEl);
-    }
-    renderHook.append(prodList);
-  },
-};
+    const addToCartBtn = productElement.querySelector('button');
+    addToCartBtn.addEventListener('click', this.addToCart.bind(this));
+    return productElement;
+  }
+}
 
-productList.render();
+class ProductList {
+  products = [
+    new Product(
+      'Trip to a Black Hole',
+      'https://res.cloudinary.com/dylvkdabj/image/upload/v1648871780/website%20pics%20family/black_hole_banner_mpkvhc.jpg',
+      'A crushing experience.',
+      '100,000.73'
+    ),
+    new Product(
+      'Fan Ride with the Guardians of the Galaxy',
+      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646514283/website%20pics%20family/wp2647127-space-wallpaper-hd_irkddn.jpg',
+      'A side splitting adventure.',
+      '100,000,765.73'
+    ),
+    new Product(
+      'Mosey on down to Natchez, Mississippi',
+      'https://res.cloudinary.com/dylvkdabj/image/upload/v1649113933/website%20pics%20family/27natchez1-jumbo_xcei5l.jpg',
+      'An ambien-like affair',
+      '00.03'
+    ),
+  ];
+  render() {
+    const productList = document.createElement('ul');
+    productList.className = 'product-list';
+    for (const product of this.products) {
+      const productItem = new ProductItem(product);
+      const productElement = productItem.render();
+      productList.append(productElement);
+    }
+    return productList;
+  }
+}
+
+class Shop {
+  render() {
+    const renderHook = document.getElementById('app');
+    this.cart = new ShoppingCart();
+    const cartElement = this.cart.render();
+
+    const productList = new ProductList();
+    const productListElement = productList.render();
+
+    renderHook.append(cartElement);
+    renderHook.append(productListElement);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
+
+// const productList = new ProductList();
+// productList.render();
+
+// const shop = new Shop();
+// shop.render();
